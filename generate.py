bc03a5312fef016781afc40e6da2fe4fa4986ce1
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-02-06 03:34:20 -0800

    5 Feb 2016: Swapped out the back end: goodbye DadaDodo, hello to my own Markove text generator.

--- generate.py
+++ generate.py
@@ -12,26 +12,30 @@ expertise to evaluate its risks, it's not for you. To put it more bluntly: THIS
 SOFTWARE IS OFFERED WITHOUT WARRANTY OF ANY KIND AT ALL.
 """
 
-
 # Thanks to https://epicjefferson.wordpress.com/2014/09/28/python-to-tumblr/ for first steps here
 
-import random
-import pprint
-import subprocess
+import random, pprint, subprocess, sys
 
 import patrick_logger    # From https://github.com/patrick-brian-mooney/personal-library
 import social_media      # From https://github.com/patrick-brian-mooney/personal-library
 from social_media_auth import autolovecraft_client
 
+
+sys.path.append('/lovecraft/markov_sentence_generator/')
+from sentence_generator import *                            # https://github.com/patrick-brian-mooney/markov-sentence-generator
+
+
 # Set up default values
 patrick_logger.verbosity_level = 2
+chains_file = '/lovecraft/chains.dat'
 
-# Functions
-
+# Utility functions
 def print_usage():    # Note that, currently, nothing calls this.
     """Print the docstring as a usage message to stdout"""
     patrick_logger.log_it("INFO: print_usage() was called")
     print(__doc__)
+
+the_markov_length, the_starts, the_mapping = read_chains(chains_file)
 
 patrick_logger.log_it("INFO: Tumblr authentication constants set up, starting run ...", 2)
 
@@ -39,28 +43,28 @@ patrick_logger.log_it("INFO: Tumblr authentication constants set up, starting ru
 the_length = 300
 patrick_logger.log_it("INFO: getting a story title ...", 2)
 while not 10 <= the_length <= 70:
-    the_title = subprocess.check_output(["dadadodo -c 1 -l /lovecraft/chains.dat -w 10000"], shell=True).decode().strip()
+    the_title = gen_text(the_mapping, the_starts, markov_length=the_markov_length, sentences_desired=1, paragraph_break_probability=0).strip()
     the_length = len(the_title)
     patrick_logger.log_it("INFO: The story title generated was '" + the_title + ".'", 2)
     patrick_logger.log_it("INFO:    And the length of that title is: " + str(the_length), 2)
     if the_title in open('/lovecraft/titles.txt').read():    # Incidentally, this is a really bad idea if the log of titles ever gets very big
         patrick_logger.log_it("That title's been used! Trying again ...\n\n\n")
-        the_length = 300    # Force the loop to grind through again
+        the_length = 300                                                            # Force the loop to grind through again
     else:
         patrick_logger.log_it("   That's a new title!", 2)
 
 patrick_logger.log_it("OK, we've got a title.\n\n", 2)
 
 
-normal_tags = 'H.P. Lovecraft, automatically generated text, Patrick Mooney, dadadodo,'
-temporary_tags = 'The Haunter of the Dark, 1935, 1936, The Haunter of the Dark Week week'
+normal_tags = 'H.P. Lovecraft, automatically generated text, Patrick Mooney, Python,'
+temporary_tags = 'The Haunter of the Dark, 1935, 1936, The Haunter of the Dark week'
 story_length = random.choice(list(range(25, 55)))
 the_content = ''
 
 patrick_logger.log_it('INFO: tags are:' + pprint.pformat(normal_tags + temporary_tags), 2)
 patrick_logger.log_it('INFO: requested story length is: ' + str(story_length) + ' sentences ... generating ...', 2)
 
-the_content = subprocess.check_output(["dadadodo -c " + str(story_length) + " -l /lovecraft/chains.dat -w 10000"], shell=True).decode()
+the_content = gen_text(the_mapping, the_starts, markov_length=the_markov_length, sentences_desired=story_length, paragraph_break_probability=0.2)
 the_lines = ["<p>" + the_line.strip() + "</p>" for the_line in the_content.split('\n\n')]
 patrick_logger.log_it("the_lines: " + pprint.pformat(the_lines))
 the_content = "\n\n".join(the_lines)
