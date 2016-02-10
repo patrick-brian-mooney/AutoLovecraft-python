d7885c43739964ed3ca327eeba8ccb1f2fe2f4c1
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-02-09 20:01:54 -0800

    9 Feb 2016: Loosened title length restrictions again.

--- generate.py
+++ generate.py
@@ -38,10 +38,10 @@ the_markov_length, the_starts, the_mapping = read_chains(chains_file)
 
 patrick_logger.log_it("INFO: Tumblr authentication constants set up, starting run ...", 2)
 
-# Next, pick out a title between 6 and 95 characters
+# Next, pick out a title between 6 and 110 characters
 the_length = 300
 patrick_logger.log_it("INFO: getting a story title ...", 2)
-while not 6 <= the_length <= 95:
+while not 6 <= the_length <= 110:
     the_title = gen_text(the_mapping, the_starts, markov_length=the_markov_length, sentences_desired=1, paragraph_break_probability=0).strip()
     the_length = len(the_title)
     patrick_logger.log_it("INFO: The story title generated was '" + the_title + ".'", 2)
