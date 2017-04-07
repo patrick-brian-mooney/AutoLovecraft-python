3e6692a565f2c6b5723dce0c6e7d7ba2a0205f25
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-04-07 03:49:23 -0700

    7 Apr 2017: changed current tags

--- generate.py
+++ generate.py
@@ -26,7 +26,7 @@ from sentence_generator import *                            # https://github.com
 
 # Set up default values
 normal_tags = 'H.P. Lovecraft, automatically generated text, Patrick Mooney, Python, Markov chains,'
-temporary_tags = 'The Colour out of Space, 1927, The Colour out of Space week'
+temporary_tags = 'The Festival, 1923, The Festival week'
 story_length = random.choice(list(range(30, 70)))
 the_content = ''
 
