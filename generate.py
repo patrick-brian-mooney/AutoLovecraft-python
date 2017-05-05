b21680250aefa39f21cc659847c9478619599bb6
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2017-05-05 12:37:07 -0700

    5 May 2017: Committing script and docs for 'The Dream Quest of Unknown Kadath' week

--- generate.py
+++ generate.py
@@ -24,7 +24,7 @@ import text_generator as tg         # https://github.com/patrick-brian-mooney/ma
 
 
 patrick_logger.verbosity_level = 2
-chains_file = '/lovecraft/chains.dat'
+chains_file = '/lovecraft/corpora/The Dream-Quest of Unknown Kadath.2.pkl'
 
 
 # Set up default values
